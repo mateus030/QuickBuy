2bb8f6c3e3901c28b010f42afc7367a08758c50d
mateus antonio <mateusasc@hotmail.com>
2019-06-24 23:22:15 -0300

    Adicionar arquivos de projeto.

---
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public abstract class Entidade
+    {
+        public List<string> _mensagensValidacao { get; set; }
+        private List<string> mensagemValidacao 
+        {
+        get {return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+        }
+
+        protected void LimparMensagensValidacao()
+        {
+            mensagemValidacao.Clear();
+        }
+
+        protected void AdicionarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+        }
+
+
+        public abstract void Validade();
+        protected bool EhValido
+        {
+         get { return !mensagemValidacao.Any(); }
+        }
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/mensagem.cs
@@ -0,0 +1,6 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    internal class mensagem
+    {
+    }
+}
